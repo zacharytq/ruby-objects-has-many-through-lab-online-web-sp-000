a0fa9776c4230cedbd6dfb64de5cb3e38171ef03
chic-page-6502 <zacharytq@gmail.com>
2019-08-15 23:56:19 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,21 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|app| app.doctor == self}
+  end
+
+  def new_appointment(date, patient)
+  end
+end
