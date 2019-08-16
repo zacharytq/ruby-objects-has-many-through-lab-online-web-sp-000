6f8bb5dc119e3a9ee0a3c6bb387a6ce71f8e66f5
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 00:15:41 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|app| app.doctor == self}
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    appointments.map {|app| app.patient}
+  end
+end
