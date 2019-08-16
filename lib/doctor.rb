267542a5b5d55d22477d371c66e62d66791f8ccb
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 00:15:29 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
     Appointment.all.select {|app| app.doctor == self}
   end
 
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     Appointment.new(date, patient, self)
   end
 
